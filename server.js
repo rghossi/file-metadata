03ea47b91d3284aac38552162199b696e725f61f
rghossi <rafael.ghossi@gmail.com>
2017-02-05 14:30:05 -0200

    finish challenge

--- server.js
+++ server.js
@@ -2,21 +2,35 @@ var express = require('express');
 var fileUpload = require('express-fileupload');
 var app = express();
 var PORT = process.env.PORT || 3000;
- 
+var fs = require("fs");
+
 app.use(fileUpload());
- 
-app.post('/upload', function(req, res) {
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  var fullAppUrl = req.protocol + '://' + req.get('host');
+  var postUrl = fullAppUrl + "/get-file-size";
+  res.render('index', { postUrl });
+});
+
+app.post('/get-file-size', function(req, res) {
   var sampleFile;
- 
   if (!req.files) {
     res.send('No files were uploaded.');
     return;
   }
  
   sampleFile = req.files.sampleFile;
- 
-  console.log(sampleFile);
-  res.json(sampleFile);
+  sampleFile.mv('uploads/' + sampleFile.name, function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+      var stats = fs.statSync('uploads/' + sampleFile.name);
+      var fileSizeInBytes = stats["size"];
+      res.json({ fileSizeInBytes });
+    }
+  }); 
 });
 
 app.listen(PORT, function () {
